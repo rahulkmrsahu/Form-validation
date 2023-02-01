f5ed8d33fc1ef2f315934ff58f7feb908101c56b
rahulkmrsahu <rahulkmrsahu@gmail.com>
2023-02-01 23:11:58 +0530

    update

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ function validation() {
     const firstNameCheck = /^[A-Za-z. ]{3,16}$/
     const lastNameCheck = /^[A-Za-z. ]{3,16}$/
     const emailCheck = /^[A-Za-z_]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z.]{2,6}$/
-    //const passwordCheck =
+    const passwordCheck = /^ (?=.*[0-9])(?=.*[!@#$%^&*._-])[A-Za-z0-9!@#$%^&*._-]6,19{}$/
     const telephoneCheck = /^[0-9]{11}$/
+    const bioCheck = /^[]/
 
     if (firstNameCheck.test(firstName)) {
         document.getElementById('first_name_error').innerHTML = ""
@@ -33,6 +34,30 @@ function validation() {
     }
     else {
         document.getElementById('email_error').innerHTML = "Email must be a vaid address, e.g.example@example.com"
+        return false
+    }
+
+    if (passwordCheck.test(password)) {
+        document.getElementById('password_error').innerHTML = ""
+    }
+    else {
+        document.getElementById('password_error').innerHTML = "Password must be alphanumeric(@._and -are also allowed)and between 6-20 character "
+        return false
+    }
+
+    if (telephoneCheck.test(telephone)) {
+        document.getElementById('telephone_error').innerHTML = ""
+    }
+    else {
+        document.getElementById('telephone_error').innerHTML = "Password must be alphanumeric(@._and -are also allowed)and between 6-20 character "
+        return false
+    }
+
+    if (bioCheck.test(yourBio)) {
+        document.getElementById('bio_error').innerHTML = ""
+    }
+    else {
+        document.getElementById('bio_error').innerHTML = "Password must be alphanumeric(@._and -are also allowed)and between 6-20 character "
         return false
     }
 
